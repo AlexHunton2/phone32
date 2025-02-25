9b7a764337606613a2a541bfb027763ee14e3876
AlexHunton2 <alexhunton02@gmail.com>
2025-02-25 15:17:26 -0500

    graphics main func

--- phone32/main/main.c
+++ phone32/main/main.c
@@ -1,13 +1,14 @@
 /* main.c */
 #include "nvs_flash.h"
 
+#include "graphics.h"
 #include "voip.h"
 
-void app_main(void)
-{
-  //Initialize NVS
+void app_main(void) {
+  // Initialize NVS
   esp_err_t ret = nvs_flash_init();
-  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
     ESP_ERROR_CHECK(nvs_flash_erase());
     ret = nvs_flash_init();
   }
